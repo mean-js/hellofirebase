4108339d10f6137a29e690b649559eb7f450d587
santosh <santosh.ece06@gmail.com>
2018-01-10 17:41:27 +0530

    firebase admin sdk.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
+let serviceAccount = require("../awesome-c4686-firebase-adminsdk-pebil-26f50db823.json");
+let admin = require("firebase-admin");
+
 let main = function() {
     try {
         console.log("Main - Hello World!");
+        console.log(serviceAccount);
+
+
     } catch (err) {
         console.log(err);
     }
