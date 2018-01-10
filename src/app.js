67808936ccbc89e717e00b8fb28957e2fd14c718
santosh <santosh.ece06@gmail.com>
2018-01-10 18:15:04 +0530

    firebase db read begin

--- src/app.js
+++ src/app.js
@@ -1,16 +1,13 @@
-let init = require("./init");
+let init = require("./init")();
 let moduleFireDb = require("./module.firebase.database");
 
 let main = function() {
     try {
         console.log("Main - Hello World!");
 
-        // initialize the singletons;
-        init();
-
         console.log("Initialzition Done!");
 
-        moduleFireDb.create();
+        moduleFireDb.read();
     } catch (err) {
         console.log(err);
     }
--- src/module.firebase.database.js
+++ src/module.firebase.database.js
@@ -1,8 +1,14 @@
 let admin = require("firebase-admin");
+let db = admin.database();
 
 let firebase = {
     "read": function() {
         console.log("READ");
+
+        let ref = db.ref("restricted_access/secret_document");
+        ref.once("value", function(snapshot) {
+            console.log(snapshot.val());
+        });
     },
 
     "readAll": function() {
